feaa099cffd8b3c9fd583edcefb95c9d43e9c9fa
osaki-mechanicbord <osaki.mf@gmail.com>
2019-07-03 22:57:37 +0900

    auto update

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,50 +1,112 @@
 $(function(){
-    function buildHTML(message){
-         image = ( message.image ) ? `<asset_path src=${message.image} >` : "";
-          var html =
-          `<div class="message">
-          <div class="upper-message">
-            <div class="upper-message__user-name">
-              ${message.user_name}
-            </div>
-            <div class="upper-message__date">
-              ${message.date}
-            </div>
-          </div>
-          <div class="lower-message">
-              <p class="lower-message__content">
-                ${message.content}
-              </p>
-          </div>
-           ${image}
-        </div>`
-        return html;
+  var buildHTML = function(message) {
+    if (message.content && message.image.url) {
+      //data-idが反映されるようにしている
+      var html = '<div class="message" data-message-id=' + message.id + '>' +
+        '<div class="upper-message">' +
+          '<div class="upper-message__user-name">' +
+            message.user_name +
+          '</div>' +
+          '<div class="upper-message__date">' +
+            message.date +
+          '</div>' +
+        '</div>' +
+        '<div class="lower-message">' +
+          '<p class="lower-message__content">' +
+            message.content +
+          '</p>' +
+          '<img src="' + message.image.url + '" class="lower-message__image" >' +
+        '</div>' +
+      '</div>'
+    } else if (message.content) {
+      //同様に、data-idが反映されるようにしている
+      var html = '<div class="message" data-message-id=' + message.id + '>' +
+        '<div class="upper-message">' +
+          '<div class="upper-message__user-name">' +
+            message.user_name +
+          '</div>' +
+          '<div class="upper-message__date">' +
+            message.date+
+          '</div>' +
+        '</div>' +
+        '<div class="lower-message">' +
+          '<p class="lower-message__content">' +
+            message.content +
+          '</p>' +
+        '</div>' +
+      '</div>'
+    } else if (message.image.url) {
+      //同様に、data-idが反映されるようにしている
+      var html = '<div class="message" data-message-id=' + message.id + '>' +
+        '<div class="upper-message">' +
+          '<div class="upper-message__user-name">' +
+            message.user_name +
+          '</div>' +
+          '<div class="upper-message__date">' +
+            message.date +
+          '</div>' +
+        '</div>' +
+        '<div class="lower-message">' +
+          '<img src="' + message.image.url + '" class="lower-message__image" >' +
+        '</div>' +
+      '</div>'
+    };
+    return html;
+  };
         
-        }
         
-        
-          $('#new_message').on('submit',function(e) {
-            e.preventDefault();
-            var formData = new FormData(this);
-            var url = $(this).attr('action');
-            $.ajax({
-              url: url,
-              type: "POST",
-              data: formData,
-              dataType: 'json',
-              processData: false,
-              contentType: false
-            })
-            .done(function(data){
+    $('#new_message').on('submit',function(e) {
+      e.preventDefault();
+      var formData = new FormData(this);
+      var url = $(this).attr('action');
+      $.ajax({
+        url: url,
+        type: "POST",
+        dataType: 'json',
+        processData: false,
+        contentType: false
+      })
+      .done(function(data){
 
-                var html = buildHTML(data);
-                  $(' .messages').append(html);
-                  $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
-                  $('#new_message')[0].reset();
-                  $(".form__submit").prop('disabled', false);
+          var html = buildHTML(data);
+            $('.messages').append(html);
+            $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+            $('#new_message')[0].reset();
+            $(".form__submit").prop('disabled', false);   
+        });
+      });
+
+        var reloadMessages = function() {
+          //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+          var last_message_id = $('.message:last').data('message-id');
+          
+          var groupId= $('.current-group').data('group_id')
+          $.ajax({
+          
+            url: `/groups/${groupId}/api/messages`,
+          
+            type: 'GET',
+            dataType: 'json',
+            
+            data: {id: last_message_id}
+          
+          })
+          .done(function(messages) {
+            
+          var html = '';
+      
+        messages.forEach(function(message){
+           html += buildHTML(message)
+        });
+
+        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+        
+      
             })
             .fail(function(){
-                alert('error');
+                console.log('error');
             });
-          });
-        });
+          }
+          
+          setInterval(reloadMessages, 5000);
+});
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,6 +5,10 @@ class MessagesController < ApplicationController
     @message = Message.new
     @messages = @group.messages.includes(:user)
     @members = @group.users
+    # respond_to do |format|
+    #   format.html
+    #   format.json { @new_messages = @messages.where('id > ?', params[:id])}
+    # end
   end
 
   def create
