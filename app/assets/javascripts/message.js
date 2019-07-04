c5f77ef94a0c3d070f795701d8172f752499f834
osaki-mechanicbord <osaki.mf@gmail.com>
2019-07-04 18:22:47 +0900

    auto update

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,59 +2,25 @@ $(function(){
   var buildHTML = function(message) {
     image = ( message.image ) ? `<asset_path src=${message.image} >` : "";
       //data-idが反映されるようにしている
-      var html = '<div class="message" data-message-id=' + message.id + '>' +
-        '<div class="upper-message">' +
-          '<div class="upper-message__user-name">' +
-            message.user_name +
-          '</div>' +
-          '<div class="upper-message__date">' +
-            message.date +
-          '</div>' +
-        '</div>' +
-        '<div class="lower-message">' +
-          '<p class="lower-message__content">' +
-            message.content +
-          '</p>' +
-          '<img src="' + message.image.url + '" class="lower-message__image" >' +
-        '</div>' +
-      '</div>'
-    } else if (message.content) {
-      //同様に、data-idが反映されるようにしている
-      var html = '<div class="message" data-message-id=' + message.id + '>' +
-        '<div class="upper-message">' +
-          '<div class="upper-message__user-name">' +
-            message.user_name +
-          '</div>' +
-          '<div class="upper-message__date">' +
-            message.date+
-          '</div>' +
-        '</div>' +
-        '<div class="lower-message">' +
-          '<p class="lower-message__content">' +
-            message.content +
-          '</p>' +
-        '</div>' +
-      '</div>'
-    } else if (message.image.url) {
-      //同様に、data-idが反映されるようにしている
-      var html = '<div class="message" data-message-id=' + message.id + '>' +
-        '<div class="upper-message">' +
-          '<div class="upper-message__user-name">' +
-            message.user_name +
-          '</div>' +
-          '<div class="upper-message__date">' +
-            message.date +
-          '</div>' +
-        '</div>' +
-        '<div class="lower-message">' +
-          '<img src="' + message.image.url + '" class="lower-message__image" >' +
-        '</div>' +
-      '</div>'
-    };
-    return html;
-  };
-        
-        
+      var html =
+      `<div class="message">
+      <div class="upper-message">
+        <div class="upper-message__user-name">
+          ${message.user_name}
+        </div>
+        <div class="upper-message__date">
+          ${message.date}
+        </div>
+      </div>
+      <div class="lower-message">
+          <p class="lower-message__content">
+            ${message.content}
+          </p>
+      </div>
+       ${image}
+    </div>`
+    return html;  
+  }  
     $('#new_message').on('submit',function(e) {
       e.preventDefault();
       var formData = new FormData(this);
